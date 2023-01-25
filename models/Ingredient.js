eadbe21b93f74f37c3e3c36d52f30153971ed197
Zenon-Nowakowski <zenon.nowakowski8@gmail.com>
2023-01-25 11:02:46 -0600

    Update to ingredients

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -4,6 +4,18 @@ const IngredientSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'please provide ingredient name'],
+    },
+    picture_content_type: {
+        type: String,
+        required: [true, 'please provide ingredient name'],
+    },
+    food_group: {
+        type: String,
+        required: [true, 'please provide ingredient name'],
+    },
+    food_subgroup: {
+        type: String,
+        required: [true, 'please provide ingredient name'],
     }
 }, { strict: false })
 
